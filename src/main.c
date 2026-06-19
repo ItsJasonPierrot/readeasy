d352b0404274f6750734c29993929c2f438347cc
Jason Pierrot <58733091+ItsJasonPierrot@users.noreply.github.com>
2026-06-20 00:27:10 +0200

    Updates
    Fixed the CLI to enable piping and arguments reading, and adding appropriate error checks

--- src/main.c
+++ src/main.c
@@ -3,57 +3,71 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]){
+  char buffer[256];
+  int input_text = STDIN_FILENO;
+
   // Argument check
-  if(argc < 2){
-    write(2,"Usage: ./readeasy <file>\n", 25);
+  if(argc > 2){
+    write(STDERR_FILENO, "Usage: readeasy <filename>\n", 27);
     return 1;
   }
-  
-  // Open file in Argument 1 (file) and check if contains anything.
-  int file = open(argv[1],O_RDONLY);
-  if(file<0){
-    write(2,"Could not locate file\n",22);
+
+  if(argc == 1 && isatty(STDIN_FILENO)){
+    write(STDERR_FILENO, "No input provided.\n",19);
     return 1;
   }
-  
+
+  if(argc == 2){
+    input_text = open(argv[1], O_RDONLY);
+    if(input_text < 0){
+      write(STDERR_FILENO, "File not found.\n", 16);
+      return 1;
+    }
+  }
+
+  ssize_t text = read(input_text,buffer,sizeof(buffer)-3);
+
   // Prepare buffer and read opened file from previous block,
   // checking if read returns either -1 or 0, and then adding 
   // newlines so that "say" doesn't stop abruptly on last word
-  char buffer[256];
-  ssize_t text = read(file,buffer,sizeof(buffer)-3); 
+
   if(text<0){
-    write(2,"Error reading file.\n", 20);
-    close(file);
+    write(STDERR_FILENO,"Error reading file.\n", 20);
+    close(input_text);
     return 1;
   }
+
   if(text==0){
-    write(2,"File is empty\n",14);
-    close(file);
+    write(STDERR_FILENO,"File is empty\n",14);
+    close(input_text);
     return 1;
   }
+
   buffer[text] = '\n';
   buffer[text+1] = '\n';
   buffer[text+2] = '\0';
 
   // Creating new child process
   pid_t pid = fork();
+
   if(pid < 0){
-    write(2,"fork failed\n",12);
-    close(file);
+    write(STDERR_FILENO,"fork failed\n",12);
+    close(input_text);
     return 1;
   }
 
   // Child process performs "say" system call with buffer from read, which perfoms TTS
   if(pid == 0){
     execlp("say","say",buffer,NULL);
-    write(2,"Child process failed\n",21);
-    close(file);
+    write(STDERR_FILENO,"Child process failed\n",21);
+    close(input_text);
     return 1;
   } else {
     wait(NULL);
   }
   
   // File is closed, and program is ended
-  close(file);
+  close(input_text);
+
   return 0;
 }
